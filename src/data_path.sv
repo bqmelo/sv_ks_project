b7ece70b93932b6f6768bf93a55f5f2ce49a9e61
bqmelo <bruna@tatango.com>
2022-11-28 23:14:35 -0300

    Add control unit

--- src/data_path.sv
+++ src/data_path.sv
@@ -21,8 +21,8 @@ import k_and_s_pkg::*;
     input  logic             [15:0] data_in
 );
 
-logic [4:0] pc_reg;
-logic [15:0] ir_reg;
+logic [4:0] program_counter;
+logic [15:0] instruction;
 logic [1:0] a_addr;
 logic [1:0] b_addr;
 logic [1:0] c_addr;
@@ -30,6 +30,7 @@ logic [4:0] mem_addr;
 logic [15:0] bus_a;
 logic [15:0] bus_b;
 logic [15:0] bus_c;
+logic [15:0] bus_a_complemento;
 logic [15:0] alu_out;
 logic flag_zero;
 logic flag_neg;
@@ -39,7 +40,7 @@ logic carry_in_ultimo_bit;
 
 always_ff @(posedge clk) begin
   if (ir_enable)
-    ir_reg <= data_in;
+    instruction <= data_in;
 end
 
 always_comb begin : decoder
@@ -48,87 +49,87 @@ always_comb begin : decoder
   c_addr = 'd0;
   mem_addr = 'd0;
 
-  case(ir_reg[15:8])
+  case(instruction[15:8])
     8'b1000_0001: begin
       decoded_instruction = I_LOAD;
-      c_addr = ir_reg[6:5];
-      mem_addr = ir_reg[4:0];
+      c_addr = instruction[6:5];
+      mem_addr = instruction[4:0];
     end
 
     8'b1000_0010: begin
-      decoded_instruction = I_LOAD;
-      a_addr = ir_reg[6:5];
-      mem_addr = ir_reg[4:0];
+      decoded_instruction = I_STORE;
+      a_addr = instruction[6:5];
+      mem_addr = instruction[4:0];
     end
 
     8'b1001_0001: begin
       decoded_instruction = I_MOVE;
-      a_addr = ir_reg[1:0];
-      b_addr = ir_reg[1:0];
-      c_addr = ir_reg[3:2];
+      a_addr = instruction[1:0];
+      b_addr = instruction[1:0];
+      c_addr = instruction[3:2];
     end
 
     8'b1010_0001: begin
       decoded_instruction = I_ADD;
-      a_addr = ir_reg[1:0];
-      b_addr = ir_reg[3:2];
-      c_addr = ir_reg[5:4];
+      a_addr = instruction[1:0];
+      b_addr = instruction[3:2];
+      c_addr = instruction[5:4];
     end
 
     8'b1010_0010: begin
       decoded_instruction = I_SUB;
-      a_addr = ir_reg[1:0];
-      b_addr = ir_reg[3:2];
-      c_addr = ir_reg[5:4];
+      a_addr = instruction[1:0];
+      b_addr = instruction[3:2];
+      c_addr = instruction[5:4];
     end
 
     8'b1010_0011: begin
       decoded_instruction = I_AND;
-      a_addr = ir_reg[1:0];
-      b_addr = ir_reg[3:2];
-      c_addr = ir_reg[5:4];
+      a_addr = instruction[1:0];
+      b_addr = instruction[3:2];
+      c_addr = instruction[5:4];
     end
 
     8'b1010_0100: begin
       decoded_instruction = I_OR;
-      a_addr = ir_reg[1:0];
-      b_addr = ir_reg[3:2];
-      c_addr = ir_reg[5:4];
+      a_addr = instruction[1:0];
+      b_addr = instruction[3:2];
+      c_addr = instruction[5:4];
     end
 
     8'b0000_0001: begin
       decoded_instruction = I_BRANCH;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_0010: begin
       decoded_instruction = I_BZERO;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_0011: begin
       decoded_instruction = I_BNEG;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_0101: begin
       decoded_instruction = I_BOV;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_0110: begin
       decoded_instruction = I_BNOV;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_1010: begin
       decoded_instruction = I_BNNEG;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b0000_1011: begin
       decoded_instruction = I_BNZERO;
-      mem_addr = ir_reg[4:0];
+      mem_addr = instruction[4:0];
     end
 
     8'b1111_1111: begin
@@ -142,36 +143,43 @@ always_comb begin : decoder
   endcase
 end
 
-always_ff @(posedge clk) begin : pc_ctrl // FF program_counter
-  if (pc_enable) begin
+always_ff @(posedge clk or negedge rst_n) begin : pc_ctrl // FF program_counter
+  if (!rst_n) begin
+    program_counter <= 'd0;
+  end
+  else if (pc_enable) begin
     if (branch)
-        pc_reg <= mem_addr;
+        program_counter <= mem_addr;
     else
-        pc_reg <= pc_reg + 1;
+        program_counter <= program_counter + 1;
     end
 end
 
-always_ff @(posedge clk) begin // Precisa do clock pois precisa do pc_reg calculado.
-  if (addr_sel) begin
-      ram_addr <= mem_addr;
-  end else begin
-      ram_addr <= pc_reg;
-  end
+always_comb begin : addr_out
+  if (addr_sel)
+      ram_addr = mem_addr; 
+  else
+      ram_addr = program_counter;
 end
 
-assign bus_c = (c_sel?data_in:alu_out);
+always_comb begin : bus_c_out
+  if (c_sel)
+    bus_c = alu_out;
+  else
+    bus_c = data_in;
+end
 
 // Banco de Registradores
 logic [15:0] r [4] = '{ default: 8'd87}; 
 
-always_ff @(posedge clk) begin
+always_ff @(posedge clk) begin 
   if(rst_n) begin
     r[0] = 15'b000000000000000;
     r[1] = 15'b000000000000000;
     r[2] = 15'b000000000000000;
     r[3] = 15'b000000000000000;
   end
-  
+
   if (write_reg_enable) begin
     case (a_addr)
       2'b00: begin
@@ -191,10 +199,7 @@ always_ff @(posedge clk) begin
       end
     endcase
 
-    data_out <= bus_a;
-
     case (b_addr)
-
       2'b00: begin
         bus_b <= r[0];
       end
@@ -207,7 +212,6 @@ always_ff @(posedge clk) begin
       2'b11: begin
         bus_b <= r[3];
       end
-
     endcase
 
     case (c_addr)
@@ -224,10 +228,12 @@ always_ff @(posedge clk) begin
         r[3] <= bus_c;
       end
     endcase
+
+    data_out = bus_a;
   end
 end
 
-always_comb begin : ula_ctr
+always_comb begin : ula_ctrl
   case(operation)
     2'b00:  begin // OR
       alu_out = bus_a | bus_b;
@@ -241,8 +247,9 @@ always_comb begin : ula_ctr
       flag_signed_overflow = flag_unsigned_overflow ^ carry_in_ultimo_bit;
     end
     2'b10: begin // SUB
-      {carry_in_ultimo_bit, alu_out[14:0]} = bus_a[14:0] - bus_b[14:0];
-      {flag_unsigned_overflow, alu_out[15]} = bus_a[15] - bus_b[15] - carry_in_ultimo_bit;
+      bus_a_complemento = (~bus_a) + 1;
+      {carry_in_ultimo_bit, alu_out[14:0]} = bus_a_complemento[14:0] + bus_b[14:0];
+      {flag_unsigned_overflow, alu_out[15]} = bus_a_complemento[15] + bus_b[15] + carry_in_ultimo_bit;
       flag_signed_overflow = flag_unsigned_overflow ^ carry_in_ultimo_bit;
     end
     default: begin // AND
